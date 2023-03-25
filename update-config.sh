9d661eb9ff2bcf8c9fd1e2cdca969e96abfd41d8
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-03-25 15:49:31 -0700

    Update update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# Define the URL of the DHCP configuration file
-DHCP_CONFIG_URL="https://raw.githubusercontent.com/LtFubuki/docker-dhcp/main/dhcpd.conf"
+# Function to fetch the latest config file from GitHub
+fetch_config() {
+  echo "Fetching configuration file..."
+  curl -L -o /etc/dhcp/dhcpd.conf --silent "${CONFIG_REPO_URL}"
+}
 
-while true
-do
-  # Download the latest version of the configuration file from GitHub
-  wget -q -O /tmp/dhcpd.conf $DHCP_CONFIG_URL
+# Fetch the initial config file
+fetch_config
 
-  # Check if the downloaded file is different from the current configuration file
-  if ! cmp -s /tmp/dhcpd.conf /etc/dhcp/dhcpd.conf; then
-    # Copy the updated configuration file to the directory
-    cp /tmp/dhcpd.conf /etc/dhcp/dhcpd.conf
-    echo "Updated DHCP configuration file"
-    # Restart the DHCP server to apply the changes
-    /etc/init.d/isc-dhcp-server restart
+# Start the DHCP server in the background
+/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf --no-pid &
+
+# Continuously poll for updates to the config file
+while true; do
+  sleep 60
+  LATEST_CONFIG=$(curl -L --silent --remote-name --remote-header-name --remote-time "${CONFIG_REPO_URL}")
+
+  # Check if the config file has changed
+  if ! cmp -s /etc/dhcp/dhcpd.conf "${LATEST_CONFIG}"; then
+    echo "Configuration file has changed, updating and restarting DHCP server..."
+    mv "${LATEST_CONFIG}" /etc/dhcp/dhcpd.conf
+
+    # Restart the DHCP server
+    pkill dhcpd
+    /usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf --no-pid &
   fi
-
-  # Wait for 5 minutes before checking for updates again
-  sleep 300
 done
